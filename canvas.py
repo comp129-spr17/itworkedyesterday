71c75a63ee8934ef721f91867ee5bfc351eaf124
jackthias <jackthias@icloud.com>
2017-02-09 14:47:32 -0800

    Follows PEP8 conventions.g

--- canvas.py
+++ canvas.py
@@ -11,25 +11,30 @@ import pprint
 import codecs
 
 '''Global variables'''
-reader = codecs.getreader('utf-8') #This exists to help the json and urllib libraries work together.
-pp = pprint.PrettyPrinter(indent=4) #Print out larger, data objects in a readable manner.
-service_url = "https://pacific.instructure.com/api/v1/" #The site that we're pulling our data from.
+reader = codecs.getreader('utf-8')  # This exists to help the json and urllib libraries work together.
+pp = pprint.PrettyPrinter(indent=4)  # Print out larger, data objects in a readable manner.
+service_url = "https://pacific.instructure.com/api/v1/"  # The site that we're pulling our data from.
 
 '''Gets the url that contains the desired data. URL changes based on the user token and the mode.
 @param mode: Represents the type of data that we're trying to recieve.'''
-def getURL(mode):
+
+
+def get_url(mode):
     return service_url + mode + '?' + parse.urlencode({'access_token': user_token})
 
-def getDataFromURL(url):
+
+def get_data_from_url(url):
     # print(url)
     response = request.urlopen(url)
     obj = json.load(reader(response))
     return obj
 
-def getData(mode):
-    return getDataFromURL(getURL(mode))
 
-def getUserToken():
+def get_data(mode):
+    return get_data_from_url(get_url(mode))
+
+
+def get_user_token():
     try:
         token_file = open('usertoken.txt', 'r')
         token = token_file.read()
@@ -39,22 +44,26 @@ def getUserToken():
         token_file.write(token)
     return token
 
-def getUserAccount():
-    mode = "users/self/profile"
-    return getData(mode)
 
-def getCourses():
+def get_user_account():
+    mode = "users/self/profile"
+    return get_data(mode)
+
+
+def get_courses():
     mode = "courses"
-    return getData(mode)
+    return get_data(mode)
+
 
 def main():
-    global user_token #The token used to autheticate the user.
-    user_token = getUserToken()
-    profile_data = getUserAccount()
-    id_number = profile_data['id'] #Can be used in place of 'self' in mode.
-    course_data = getCourses()
+    global user_token  # The token used to authenticate the user.
+    user_token = get_user_token()
+    profile_data = get_user_account()
+    id_number = profile_data['id']  # Can be used in place of 'self' in mode.
+    course_data = get_courses()
     pp.pprint(course_data)
     for course in course_data:
         print(course['name'])
+
 
 main()
