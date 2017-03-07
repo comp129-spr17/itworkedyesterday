09a358e71c588b66b5517912bccd602b7bbf1931
i-tran1 <i_tran1@u.pacific.edu>
2017-03-06 20:00:04 -0800

    Functions made for create, insert, and access for user and course table

--- database.py
+++ database.py
@@ -1,5 +1,4 @@
-import mysql.connector
-from mysql.connector import errorcode
+import sqlite3
 import data_structures
 import canvas
 
@@ -8,52 +7,50 @@ def main():
     TABLES = {}
 
 def connect_to_db():
+    """
     username = 'admin'
     pw = 'mypassword'
     host = 'canvasplusplus.cm39236m2xbo.us-west-2.rds.amazonaws.com'
     db_name = 'canvas++'
-    try:
-        connection = mysql.connector.connect(user=username, password=pw, host=host, databse=db_name)
-    except mysql.connector.Error as err:
-        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            print("ERROR: Username or Password is not correct")
-        elif err.errno == errorcode.ER_BAD_DB_ERROR:
-            print("ERROR: Database does not exist")
-        else:
-            print(err)
-    else:
-        connection.close()
-
-
-def create_user_table(TABLES):
     """
+
+def create_connection(conn):
     #cursor object
     c=conn.cursor()
+    return c
+
+
+def create_user_table(c,conn):
     #create table for user
     c.execute ('''CREATE TABLE users (id, name, account_id, enrollment_term_id)''')
     #save (commit) the changes to database
     conn.commit()
-    """
-    TABLES['user'] = (
-        "CREATE TABLE 'user' ("
-        "'user_id' int(9) NOT NULL,"
-        #how long do we need to make names?
-        "'user_name varchar(30) NOT NULL,"
-        #how many numbers are in account id?
-        "'account_id' int(10) NOT NULL,"
-        #how many numbers are in enrollment term id?
-        "'enrollment_term_id int(10) NOT NULL,"
-        "PRIMARY KEY ('user_id')"
-        ") ENGINE = InnoDB"
-    )
 
-def insert_user(user):
+
+def insert_user(c, user, conn):
     #insert user into table
     c.execute("INSERT INTO user VALUES (user.id, user.data, user.bio, user.avatar_url, user.login_id)")
     #save (commit) the changes to database
     conn.commit()
 
-def access_user(user):
+def access_user(c,user):
     #access user information from user table by user ID
     c.execute('SELECT user.id FROM user')
+
+def create_course_table(c, conn):
+    #create course table
+    c.execute('''CREATE TABLE courses (canvas_id, course_name, associated_account, term)''')
+    #save (commit) the changes to databse
+    conn.commit()
+
+def insert_course(c, user, conn):
+    #insert course into table
+    #need to pass in correct way to access this info about courses
+    c.execute("INSERT INTO courses (user.canvas_id, user.name, user.canvas_account, user.canvas_term")
+    #save(commit) the changes to database
+    conn.commit()
+
+def access_user_courses(c, user):
+    c.execute('SELECT user.canvas_id FROM course')
+
 main()
