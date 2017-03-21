cb1fd9c1465c9fd927f04774746b47fd76058c49
jackthias <jackthias@icloud.com>
2017-03-21 13:59:30 -0700

    Changes made for the presentation.

--- canvasplusplus/urls.py
+++ canvasplusplus/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from tasks.views import view_tasks
+from tasks.views import view_tasks, view_completed
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^tasks/$', view_tasks)
+    url(r'^tasks/$', view_tasks),
+    url(r'^tasks/completed/$', view_completed)
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -6,11 +6,20 @@ from tasks.models import DB_User, DB_TodoList, DB_Tasks
 
 # Create your views here.
 
-user = DB_User.objects.get(username="Sterling_Archer")
+# user = DB_User.objects.get(username="Sterling_Archer")
+user = DB_User.objects.get(username="Arthur_Dent")
 
 def view_tasks(request):
     f = open('html/tasks.html')
     template = get_template('tasks.html')
-    context = Context({'todos': DB_Tasks.objects.filter(user=user.id)})
+    context = Context({'todos': DB_Tasks.objects.filter(user=user.id, completed=False), 'username': user.username, 'imgurl': user.canvas_avatar_url})
+    html = template.render(context)
+    return HttpResponse(html)
+
+
+def view_completed(request):
+    f = open('html/tasks.html')
+    template = get_template('completed.html')
+    context = Context({'todos': DB_Tasks.objects.filter(user=user.id, completed=True), 'username': user.username, 'imgurl': user.canvas_avatar_url})
     html = template.render(context)
     return HttpResponse(html)
