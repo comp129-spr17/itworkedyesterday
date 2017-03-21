f7214d22e95b1829d96cc986e48aba93a6614e93
jackthias <jackthias@icloud.com>
2017-03-20 23:03:25 -0700

    Configured Django settings and updated html to use Django templates.

--- canvasplusplus/settings.py
+++ canvasplusplus/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'canvasplusplus.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'cppdb.c1plntkoobsn.us-west-2.rds.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
