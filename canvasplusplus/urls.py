cd6fbb27f5944ad1e1ea712c403a2349cd45ebb8
jackthias <jackthias@icloud.com>
2017-03-26 23:24:13 -0700

    Created Add, Edit, Complete(/uncomplete), and Delete functions.

--- canvasplusplus/urls.py
+++ canvasplusplus/urls.py
@@ -21,8 +21,8 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^tasks/$', views.view_tasks),
     url(r'^tasks/completed/$', views.view_completed),
-    url(r'^([.+/]?)rem/([0-9]+)/([0-9]+)$)', views.removed_task),
-    url(r'^([.+/]?)com/([0-9]+)/([0-9]+)$)', views.complete_task),
-    url(r'^([.+/]?)edt/([0-9]+)/([0-9]+)$)/(.+)', views.edit_task),
+    url(r'^([.+/]?)rem/([0-9]+)/([0-9]+)/$)', views.removed_task),
+    url(r'^([.+/]?)com/([0-9]+)/([0-9]+)/$)', views.complete_task),
+    url(r'^([.+/]?)edt/([0-9]+)/([0-9]+)$)/(.+)/$', views.edit_task),
     url(r'^([.+/]?)add/([0-9]+)/([0-9]+)$)/(.+)/$)', views.add_task)
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from datetime import datetime
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template.loader import get_template
 from django.template import Context
@@ -26,16 +27,44 @@ def view_completed(request):
     return HttpResponse(html)
 
 
+def handle_source(source):
+    if source != "":
+        return redirect(source)
+    else:
+        return redirect('/tasks/')
+
+
+def create_task(user, list_id, name):
+    return DB_Tasks(user=user, task_name=name, todo_list=list_id, category="Default")
+
+
+def get_task(user_id, task_id):
+    return DB_Tasks.objects.get(user=user_id, id=task_id)
+
+
 def removed_task(request, source, user_id, task_id):
-    
+    selected_task = get_task(user_id, task_id)
+    if selected_task is not None:
+        selected_task.delete()
+    return handle_source(source)
 
 
 def complete_task(request, source, user_id, task_id):
-
+    selected_task = get_task(user_id, task_id)
+    if selected_task is not None:
+        selected_task.completed = not selected_task.completed
+    return handle_source(source)
 
 
 def edit_task(request, source, user_id, task_id, new_name):
-
+    selected_task = get_task(user_id, task_id)
+    if selected_task is not None:
+        selected_task.task_name = new_name
+        selected_task.save()
+    return handle_source(source)
 
 
 def add_task(request, source, user_id, list_id, new_task):
+    task = DB_Tasks(user_id, list_id, new_task)
+    task.save()
+    return handle_source(source)
