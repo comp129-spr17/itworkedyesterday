02ce33d05b8686e2c0e0787b94248c8795eca207
jackthias <jackthias@icloud.com>
2017-03-27 17:50:12 -0700

    Fixed some issues with the task and completed views.

--- tasks/views.py
+++ tasks/views.py
@@ -14,7 +14,10 @@ user = DB_User.objects.get(username="Arthur_Dent")
 def view_tasks(request):
     f = open('html/tasks.html')
     template = get_template('tasks.html')
-    context = Context({'todos': DB_Tasks.objects.filter(user=user.id, completed=False), 'username': user.username, 'imgurl': user.canvas_avatar_url})
+    context = Context({'todos': DB_Tasks.objects.filter(user=user.id, completed=False),
+                       'username': user.username,
+                       'imgurl': user.canvas_avatar_url,
+                       'list': get_template('list.html')})
     html = template.render(context)
     return HttpResponse(html)
 
@@ -22,7 +25,10 @@ def view_tasks(request):
 def view_completed(request):
     f = open('html/tasks.html')
     template = get_template('completed.html')
-    context = Context({'todos': DB_Tasks.objects.filter(user=user.id, completed=True), 'username': user.username, 'imgurl': user.canvas_avatar_url})
+    context = Context({'todos': DB_Tasks.objects.filter(user=user.id, completed=True),
+                       'username': user.username,
+                       'imgurl': user.canvas_avatar_url,
+                       'list': get_template('list.html')})
     html = template.render(context)
     return HttpResponse(html)
 
@@ -53,6 +59,7 @@ def complete_task(request, source, user_id, task_id):
     selected_task = get_task(user_id, task_id)
     if selected_task is not None:
         selected_task.completed = not selected_task.completed
+        selected_task.save()
     return handle_source(source)
 
 
