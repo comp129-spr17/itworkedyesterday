be587e56f9c790a97f0c309fe8ca4c05cf9d7355
Kashper20 <k_hartman@u.pacific.edu>
2017-04-04 13:08:02 -0700

    register is working

--- tasks/views.py
+++ tasks/views.py
@@ -29,7 +29,7 @@ def signup(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect('home')
+            return redirect('/login/')
     else:
         form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
