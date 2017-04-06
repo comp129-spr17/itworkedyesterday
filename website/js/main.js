8337bcd8f169ab59320dea81b07a3545a2ee50e2
Alex <a_hall11@u.pacific.edu>
2017-04-05 18:31:26 -0700

    deleted usless files.

--- website/js/main.js
+++
@@ -1,9 +0,0 @@
-
-function addTask() {
-	var task = document.getElementById("titleInput").value
-	var text = document.createTextNode(task)
-	var newTask = document.createElement("li")
-
-	newTask.appendChild(text)
-	document.getElementById("todoList").appendChild(newTask)
-}
