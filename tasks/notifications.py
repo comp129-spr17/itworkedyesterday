25fd407c5b88b6e289ca9e4f3d61b7cb15c4e335
Clement <clementgarcia247@gmail.com>
2017-04-26 17:51:18 -0700

    Added method for sending email notifications and deleted email.py

--- tasks/notifications.py
+++ tasks/notifications.py
@@ -4,6 +4,9 @@ from django_cron import CronJobBase, Schedule
 
 from .models import DB_Due, DB_Tasks, DB_User
 
+from django.core.mail import send_mail
+
+from django.contrib.auth.models import User
 
 class MyCronJob(CronJobBase):
     RUN_EVERY_MINS = 5
@@ -20,6 +23,15 @@ def fire_safari(task):
 
 
 def fire_email(task):
+    user = User.objects.get(id=task.user)
+    email = user.email
+    send_mail(
+        'Canvas++ Notification',
+        'REMINDER: ' + task.task_name + ' is due by ' + task.end_date,
+        'canvasplusplus@gmail.com',
+        [email],
+        fail_silently=False,
+    )
     pass
 
 
